85dcc109f7b8d5cdd67af7857760fc47f07ff4e8
Nilay <54245048+dnilay@users.noreply.github.com>
2020-06-16 11:47:15 +0530

    Add files via upload

---
+++ school-service/src/main/java/com/example/demo/SchoolServiceApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+public class SchoolServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolServiceApplication.class, args);
+	}
+
+}
