2bbc0bafe0fb89663712526ddb909e109988674b
Nilay <54245048+dnilay@users.noreply.github.com>
2020-06-17 14:57:39 +0530

    Add files via upload

---
+++ spring-boot-rest-basic-security/src/main/java/com/example/springbootrestbasicsecurity/SpringBootRestBasicSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootrestbasicsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestBasicSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestBasicSecurityApplication.class, args);
+    }
+
+}
---
+++ spring-boot-rest-basic-security/src/test/java/com/example/springbootrestbasicsecurity/SpringBootRestBasicSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootrestbasicsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestBasicSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
